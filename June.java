ba2e927d4af0af7f57e9a9a6990478ee1a8ec53c
Jack Sawyer <84543199+JackSawyerWATX@users.noreply.github.com>
2022-09-03 09:32:52 -0700

    additions

--- June.java
+++ June.java
@@ -3,10 +3,9 @@ public class June {
     String also;
     String and;
     
-
     public June() {
         hey = "Will you make martinis in the new shaker?";
-        also = " I want vodka in mine please, not gin ";
+        also = " I want vodka in mine please, not gin.";
         and = "Oh! And three olives please!";
     }
 
@@ -16,31 +15,5 @@ public class June {
         System.out.println(ques.also);
         System.out.print(ques.and);
     }
-    public static void week() {
-    String day = "Thursday";
-    switch (day) {
-        case Monday;
-        System.out.println("Get ready for school!");
-        break;
-        case Tuesday;
-        System.out.println("Get ready for school!");
-        break;
-        case Wednesday;
-        System.out.println("Get ready for school!");
-        break; 
-        case Thursday;
-        System.out.println("Get ready for school!");
-        break;
-        case Friday;
-        System.out.println("Get ready for school!");
-        break;
-        case Saturday;
-        System.out.println("It's the weekend!");
-        case Sunday;
-        System.out.println("It's the weekend!");
-        break;
-        default:
-        System.out.println("Come eat breakfast.");
-    }
-}
+    
 }
